2f12b419cc6c547dd85b2c7cdff1290955b4963f
hemant.bairwa <hemantbairwa86@gmail.com>
2026-09-26 02:05:58 +0530

    chnage

--- application/views/administrator/common/header.php
+++ application/views/administrator/common/header.php
@@ -4,36 +4,35 @@
       <img src="<?php echo base_url(); ?>assest/frontend/images/logo.svg" alt="logo" style="height: 30px; max-width: 140px; width: auto; background: #ffffff; padding: 3px 8px; border-radius: 6px; box-shadow: 0 2px 6px rgba(0,0,0,0.12);"/>
       <span class="brand-admin-tag ml-2">Admin</span>
     </a>
-    <a class="navbar-brand brand-logo-mini" href="<?php echo base_url(); ?>administrator/dashboard">
+    <a class="navbar-brand brand-logo-mini d-flex align-items-center justify-content-center" href="<?php echo base_url(); ?>administrator/dashboard">
       <span class="mini-vp-badge">VP</span>
     </a>
   </div>
-  <div class="navbar-menu-wrapper d-flex align-items-stretch">
-    <button class="navbar-toggler navbar-toggler align-self-center text-white" type="button" data-toggle="minimize">
-      <span class="mdi mdi-menu" style="font-size: 1.4rem;"></span>
-    </button>
-    <ul class="navbar-nav">
-      <li class="nav-item d-none d-lg-block">
-        <a class="nav-link text-white" title="Toggle Fullscreen">
+  <div class="navbar-menu-wrapper d-flex align-items-center justify-content-between">
+    <div class="d-flex align-items-center">
+      <button class="navbar-toggler navbar-toggler align-self-center text-white d-none d-lg-block" type="button" data-toggle="minimize" title="Toggle Sidebar">
+        <span class="mdi mdi-menu" style="font-size: 1.4rem;"></span>
+      </button>
+      <a href="<?php echo base_url(); ?>" target="_blank" class="btn btn-sm btn-header-visit ml-2 d-none d-sm-inline-flex align-items-center" title="Visit Storefront">
+        <i class="fa fa-globe mr-1"></i> <span class="d-none d-md-inline">Visit Storefront</span>
+      </a>
+    </div>
+
+    <ul class="navbar-nav navbar-nav-right d-flex align-items-center flex-row">
+      <li class="nav-item d-none d-lg-block mr-2">
+        <a class="nav-link text-white p-2" title="Toggle Fullscreen" href="javascript:void(0);">
           <i class="mdi mdi-fullscreen" id="fullscreen-button" style="font-size: 1.3rem;"></i>
         </a>
       </li>
-      <li class="nav-item d-none d-md-block ml-3 align-self-center">
-        <a href="<?php echo base_url(); ?>" target="_blank" class="btn btn-sm btn-header-visit">
-          <i class="fa fa-globe mr-1"></i> Visit Storefront
-        </a>
-      </li>
-    </ul>
-    <ul class="navbar-nav navbar-nav-right">
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle nav-profile d-flex align-items-center" id="profileDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
           <div class="profile-img-wrap" style="position: relative;">
-            <img src="<?php echo base_url(); ?>assest/administrator/images/faces/default_male.jpg" alt="profile" style="width: 36px; height: 36px; border-radius: 50%; border: 2px solid rgba(255,255,255,0.85); object-fit: cover;">
+            <img src="<?php echo base_url(); ?>assest/administrator/images/faces/default_male.jpg" alt="profile" style="width: 34px; height: 34px; border-radius: 50%; border: 2px solid rgba(255,255,255,0.85); object-fit: cover;">
             <span class="online-status-dot"></span>
           </div>
-          <span class="d-none d-lg-inline text-white font-weight-bold ml-2" style="font-size: 0.95rem;"><?php echo $this->session->userdata('VenusProductSession')->name; ?></span>
+          <span class="d-none d-md-inline text-white font-weight-bold ml-2" style="font-size: 0.9rem;"><?php echo isset($this->session->userdata('VenusProductSession')->name) ? $this->session->userdata('VenusProductSession')->name : 'Admin'; ?></span>
         </a>
-        <div class="dropdown-menu navbar-dropdown dropdown-menu-right" aria-labelledby="profileDropdown">
+        <div class="dropdown-menu navbar-dropdown dropdown-menu-right shadow-lg border-0" aria-labelledby="profileDropdown">
           <a class="dropdown-item" href="<?php echo base_url(); ?>administrator/profile">
             <i class="mdi mdi-account-circle mr-2 text-success" style="font-size:1.1rem;"></i> My Profile
           </a>
@@ -47,9 +46,11 @@
           </a>
         </div>
       </li>
+      <li class="nav-item d-lg-none ml-2">
+        <button class="navbar-toggler navbar-toggler-right align-self-center text-white border-0 p-2" type="button" data-toggle="offcanvas" aria-label="Toggle Menu">
+          <span class="mdi mdi-menu" style="font-size: 1.5rem;"></span>
+        </button>
+      </li>
     </ul>
-    <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center text-white" type="button" data-toggle="offcanvas">
-      <span class="mdi mdi-menu"></span>
-    </button>
   </div>
 </nav>
