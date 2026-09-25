86c98a8a0b17c08f370310a91890d913d8cfc99a
hemant.bairwa <hemantbairwa86@gmail.com>
2026-09-26 01:08:22 +0530

    Fix dynamic sidebar JavaScript matching and non-active menu item CSS styles

--- assest/administrator/js/misc.js
+++ assest/administrator/js/misc.js
@@ -8,30 +8,29 @@
     var sidebar = $('.sidebar');
 
     //Add active class to nav-link based on url dynamically
-    //Active class can be hard coded directly in html file also as required
-    var current = location.pathname.split("/").slice(-1)[0].replace(/^\/|\/$/g, '');
-    $('.nav li a', sidebar).each(function() {
-      var $this = $(this);
-      if (current === "") {
-        //for root url
-        if ($this.attr('href').indexOf("index.html") !== -1) {
-          $(this).parents('.nav-item').last().addClass('active');
+    var pathSegments = location.pathname.split("/").filter(Boolean);
+    var current = pathSegments.length > 0 ? pathSegments[pathSegments.length - 1] : '';
+    if (current === 'administrator') {
+      current = 'dashboard';
+    }
+
+    if (current !== "") {
+      $('.nav li a', sidebar).each(function() {
+        var href = $(this).attr('href');
+        if (!href) return;
+        var hrefSegments = href.split("/").filter(Boolean);
+        var lastHrefSegment = hrefSegments.length > 0 ? hrefSegments[hrefSegments.length - 1] : '';
+
+        if (lastHrefSegment === current) {
+          $(this).closest('.nav-item').addClass('active');
           if ($(this).parents('.sub-menu').length) {
             $(this).closest('.collapse').addClass('show');
             $(this).addClass('active');
+            $(this).closest('.collapse').parent('.nav-item').addClass('active');
           }
         }
-      } else {
-        //for other url
-        if ($this.attr('href').indexOf(current) !== -1) {
-          $(this).parents('.nav-item').last().addClass('active');
-          if ($(this).parents('.sub-menu').length) {
-            $(this).closest('.collapse').addClass('show');
-            $(this).addClass('active');
-          }
-        }
-      }
-    })
+      });
+    }
 
     //Close other submenu in sidebar on opening any
 
